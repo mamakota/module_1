964281ac617958c54b9c8a85358c01984ad6637e
Ilya Chuchkov <mamakoshki95@gmail.com>
2022-07-07 11:55:30 +0300

    game rework

--- lesson_12/game_01.js
+++ lesson_12/game_01.js
@@ -1,28 +1,33 @@
-'use strict'
+'use strict';
 
 const randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
+
 const game = () => {
-  let yourTake = prompt('enter number 1-100', '');
+  const yourTake = +prompt('enter number 1-100', '');
+  let win = false;
+
   switch (true) {
-    case (yourTake === ''):
-      game();
-      break;
-    case (yourTake === null):
+    case (yourTake === null || yourTake === ''):
       break;
     case (yourTake < randomNumber):
       alert('Больше');
-      game();
       break;
     case (yourTake > randomNumber):
       alert('Меньше');
-      game();
       break;
     case (+yourTake === randomNumber):
       alert('Правильно!');
+      win = true;
       break;
   }
-}
+
+  if (win) {
+    return yourTake;
+  } else {
+    return game();
+  }
+};
 
 // console.log(null === null);
 
