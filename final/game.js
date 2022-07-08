41dce00e33df2742ebebc550b106d19def12623c
Ilya Chuchkov <mamakoshki95@gmail.com>
2022-07-08 17:19:31 +0300

    final edit

--- final/game.js
+++ final/game.js
@@ -9,13 +9,13 @@
 
   const game = () => {
     const bag = {
-      player: 5,
-      computer: 5,
+      player: 15,
+      computer: 15,
     };
 
     return function start() {
       const randomNumber = getRandomIntInclusive(0, 1);
-      const promptPlayer = +prompt(`Введите число от 1 до ${bag.player}`, '');
+      const promptPlayer = prompt(`Введите число от 1 до ${bag.player}`, '');
 
       if (randomNumber === 0) {
         console.log('бот загадал четное');
@@ -23,14 +23,18 @@
         console.log('бот загадал нечетное');
       }
 
-      if (!(promptPlayer === null) && !(promptPlayer === '') && bag.player > 0 && bag.computer > 0) {
+      if (!(promptPlayer === null) && !(promptPlayer === '') && bag.player > 0 && bag.computer > 0 && !isNaN(promptPlayer)) {
 
         if (promptPlayer % 2 === randomNumber) {
-          bag.player -= promptPlayer;
-          bag.computer += promptPlayer;
+          bag.player -= +promptPlayer;
+          bag.computer += +promptPlayer;
+          alert('bot won');
+          alert(`Score:\nBot:${bag.computer}\nPlayer:${bag.player}`);
         } else {
-          bag.player += promptPlayer;
-          bag.computer -= promptPlayer;
+          bag.player += +promptPlayer;
+          bag.computer -= +promptPlayer;
+          alert('you won');
+          alert(`Score:\nBot:${bag.computer}\nPlayer:${bag.player}`);
         }
 
       }
